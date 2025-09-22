221b6e557e2fc3f9f7855d020ba0f36e7e43d9d0
Yusuf Wijaya <yusuf.wijaya@esb.co.id>
2025-09-22 14:05:48 +0700

    feat: refactor

--- constants/constant.go
+++ constants/constant.go
@@ -1,22 +1,8 @@
 package constants
 
 const (
-	Sub               = "sub"
-	UserName          = "username"
-	FullName          = "fullName"
-	CompanyID         = "companyID"
-	CompanyCode       = "companyCode"
-	CompanyName       = "companyName"
-	UserRoleID        = "userRoleID"
-	DBName            = "dbName"
-	ServerCode        = "serverCode"
-	DBMain            = "main"
-	DateFormat        = "2006-01-02"
-	DateTimeFormat    = "2006-01-02 15:04:05"
-	ExcelBaseDate     = 25569
-	SecondsInADay     = 86400
-	ESBFnbDB          = "esb_fnb"
-	DefaultMailgunTag = "ERP"
-	SentryCompanyCode = "company.code"
-	SentryServerCode  = "server.code"
+	UserName       = "username"
+	FullName       = "fullName"
+	DateFormat     = "2006-01-02"
+	DateTimeFormat = "2006-01-02 15:04:05"
 )
